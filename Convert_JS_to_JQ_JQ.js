aa99f8c9fc1f953f3ea79bd61dbc9f1af0daaf79
Your Name <you@example.com>
2020-09-13 20:04:23 -0400

    first commit

---
+++ Convert_JS_to_JQ_JQ.js
@@ -0,0 +1,10 @@
+$('.res').alertHover( function(){
+  $( "area" ).css("backgroundColor", "green");
+    alert("Close me to turn the box green.");
+});
+  
+    $('.res').click( function(){
+     $("#area").css("backgroundColor", "#953674");
+
+
+  });
